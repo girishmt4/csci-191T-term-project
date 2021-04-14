7924107bb16d0de8f0e63bba803ee11ca19dee31
manmohanml1 <manmohanlonawat@gmail.com>
2021-04-14 11:14:58 -0700

    New class for Screen settings

---
+++ TermProject/include/_screenSettings.h
@@ -0,0 +1,16 @@
+#ifndef _SCREENSETTINGS_H
+#define _SCREENSETTINGS_H
+
+
+class _screenSettings
+{
+    public:
+        _screenSettings();
+        virtual ~_screenSettings();
+
+    protected:
+
+    private:
+};
+
+#endif // _SCREENSETTINGS_H
---
+++ TermProject/src/_screenSettings.cpp
@@ -0,0 +1,11 @@
+#include "_screenSettings.h"
+
+_screenSettings::_screenSettings()
+{
+    //ctor
+}
+
+_screenSettings::~_screenSettings()
+{
+    //dtor
+}
